f6083e2fd9be122f7c1475dec779adfe26fd0d31
Daniel Martinez <dmartinez6319@gmail.com>
2025-02-08 17:04:42 -0500

    vercel json 3

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // This should be the default, but confirm.
+  },
 })
